e18f650256769b3ef73d9f05afc9da4fa76dfeba
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-07-09 21:44:06 +0200

    Add taxi bus config

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Command = 'taxibus'
+Config.VehicleModel = 'tourbus'
+
+Config.Plate = 'LSTAXI'
+
+-- Taxi-Gelb / Schwarz. Kann später angepasst werden.
+Config.PrimaryColor = 88
+Config.SecondaryColor = 0
+
+Config.WarpIntoDriverSeat = true
+Config.DeleteOldTaxibus = true
