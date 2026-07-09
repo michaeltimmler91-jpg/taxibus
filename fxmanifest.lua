52781eaacb0f3d6bfc1833aec6f019e3c40ce479
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-07-09 21:43:50 +0200

    Switch resource to taxi bus command

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 
-name 'lst_shuttle'
+name 'lst_taxibus'
 author 'Los Santos Taxi / Michael Timmler'
-description 'Los Santos Taxi Shuttle based on GTA V Tourbus'
-version '1.0.0'
+description 'Taxi-Shuttle command using the original GTA V tourbus'
+version '1.1.0'
 
-files {
-    'data/vehicles.meta',
-    'data/carvariations.meta',
-    'data/carcols.meta',
-    'data/handling.meta'
+client_scripts {
+    'config.lua',
+    'client.lua'
 }
-
-data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
-data_file 'CARCOLS_FILE' 'data/carcols.meta'
-data_file 'HANDLING_FILE' 'data/handling.meta'
-
-client_script 'vehicle_names.lua'
